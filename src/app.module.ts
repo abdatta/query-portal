2cf239a7e873645cd3c234173941eb74c49c68dc
Abhishek Datta <abdatta@iitk.ac.in>
2020-10-27 14:55:15 +0530

    Serve static frontend build from backend

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,7 @@
+import { join } from 'path';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { ServeStaticModule } from '@nestjs/serve-static';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -8,6 +10,9 @@ import { QueryModule } from './query/query.module';
 @Module({
   imports: [
     ConfigModule.forRoot(),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, 'public'),
+    }),
     MongooseModule.forRoot(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useFindAndModify: true,
